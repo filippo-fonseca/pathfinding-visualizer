9c1650b5913fd70b06f3b0586f91ab9553655f76
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-01-23 16:37:04 -0600

    proper initial setup

---
+++ src/PathfindingVisualizer/Node/Node.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Node = () => {
+  return <div className='node'>Foo</div>;
+};
+
+export default Node;
---
+++ src/PathfindingVisualizer/PathfindingVisualizer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Node from './Node/Node';
+
+const PathfindingVisualizer = () => {
+  return (
+    <div>
+      <Node />
+    </div>
+  );
+};
+
+export default PathfindingVisualizer;
